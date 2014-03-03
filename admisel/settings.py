ab2f27e721c345a09608b2eaefd02425a8b794c9
omar <omarisel01@gmail.com>
2014-03-03 12:42:54 -0600

    data

--- admisel/settings.py
+++ admisel/settings.py
@@ -122,6 +122,8 @@ ROOT_URLCONF = 'admisel.urls'
 WSGI_APPLICATION = 'admisel.wsgi.application'
 
 TEMPLATE_DIRS = (
+ os.path.join(os.path.dirname(__file__),'templates'),
+
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
